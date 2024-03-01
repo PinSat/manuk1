aaf165e0fcfd41002830e031b216a7aef0a5ee58
PinSat <161663251+PinSat@users.noreply.github.com>
2024-03-01 10:45:53 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1709264499,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundflappybird-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/frame2-sheet0.png",
+		"images/frame2-sheet1.png",
+		"images/frame2-sheet2.png",
+		"images/bwhsheet-sheet0.png",
+		"images/atassheet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
